0f3b7825576c0389cb03cd17e5262b877c6a1541
DhjAsnani <asnani.dheeraj@yahoo.com>
2016-05-17 10:28:35 +0530

    first commit

---
+++ app/src/main/java/dhj/SendImage/MainActivity.java
@@ -0,0 +1,82 @@
+package dhj.SendImage;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+
+    final int RQS_LOADIMAGE = 0;
+    final int RQS_SENDEMAIL = 1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void process(View view) {
+
+        if(view.getId()==R.id.sendImage)
+        {
+            Intent intent = null,chooser = null;
+            intent = new Intent(Intent.ACTION_SEND);
+            Uri ImageUri = Uri.parse(getString(R.string.done)+R.mipmap.ic_launcher);
+            intent.setType("image/*");
+            intent.putExtra(Intent.EXTRA_STREAM, ImageUri);
+            intent.putExtra(Intent.EXTRA_TEXT,"ye mne bheja hai");
+            chooser = Intent.createChooser(intent,"LaunchMe");
+            startActivity(chooser);
+        }
+        if(view.getId()==R.id.doit)
+        {
+            Intent intent = new Intent(Intent.ACTION_PICK,
+                    android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(intent, RQS_LOADIMAGE);
+        }
+    }
+}
